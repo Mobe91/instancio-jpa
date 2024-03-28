5457f363114d8fc1dd6abd6388bc51e67792ce9b
Moritz Becker <moritz.becker@gmx.at>
2024-03-28 15:03:40 +0100

    Update license headers

--- javax/src/main/java/com/mobecker/instancio/jpa/EntityGraphAssociationFixer.java
+++ javax/src/main/java/com/mobecker/instancio/jpa/EntityGraphAssociationFixer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 - 2023 Moritz Becker.
+ * Copyright 2023 - 2024 Moritz Becker.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
